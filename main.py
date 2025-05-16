3d37e5325893616cf01e979f34d43a662222d418
Daniil Kr <drobov@insports.tv>
2025-05-16 12:19:21 +0300

    Refactoring the code. Adding a readme

--- main.py
+++ main.py
@@ -7,29 +7,28 @@ import telegram
 from dotenv import load_dotenv
 
 
-def get_image(url):
+COMICS_FILENAME = "xkcd.png"
+COMICS_MIN_NUMBER = 1
+COMICS_MAX_NUMBER = 3090
+
+
+def upload_comics(file_to_publish, comics_number):
+    url = f"https://xkcd.com/{comics_number}/info.0.json"
     response = requests.get(url)
     response.raise_for_status()
+    comics_response = response.json()
 
-    return response.content
-
-
-def upload_comic(file_to_publish):
-    comic_number = random.randint(1, 3090)
-    url = f"https://xkcd.com/{comic_number}/info.0.json"
-    response = requests.get(url)
+    response = requests.get(comics_response["img"])
     response.raise_for_status()
-    comic_response = response.json()
 
-    img_comic = get_image(comic_response["img"])
     with open(file_to_publish, "wb") as file:
-        file.write(img_comic)
+        file.write(response.content)
 
-    return comic_response["alt"]
+    return comics_response["alt"]
 
 
-def publish_comic(chat_id, bot, comment):
-    with open("xkcd.png", "rb") as file:
+def publish_comics(chat_id, bot, comment):
+    with open(COMICS_FILENAME, "rb") as file:
         bot.send_photo(chat_id=chat_id, photo=file, caption=comment)
 
 
@@ -40,17 +39,16 @@ def main():
 
     bot = telegram.Bot(token=token)
 
-    file_to_publish = "xkcd.png"
-
     daily_publication = 86400
 
     while True:
-        comment = upload_comic(file_to_publish)
+        comics_number = random.randint(COMICS_MIN_NUMBER, COMICS_MAX_NUMBER)
+        comment = upload_comics(COMICS_FILENAME, comics_number)
 
-        publish_comic(chat_id, bot, comment)
+        publish_comics(chat_id, bot, comment)
 
-        if os.path.exists(file_to_publish):
-            os.remove(file_to_publish)
+        if os.path.exists(COMICS_FILENAME):
+            os.remove(COMICS_FILENAME)
 
         time.sleep(daily_publication)
 
